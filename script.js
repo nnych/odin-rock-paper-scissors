254e78fe3b24050773ef74a43f25f948c67c409b
Nathan Nychyporuk <121582694+nnych@users.noreply.github.com>
2024-07-27 09:41:26 -0700

    style html and set up computer choice

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-console.log('Hello World');
-console.log('Another hello.')
+function getComputerChoice() {
+  //return a random rock paper scissors choice
+  const rand = Math.random();
+
+  if (rand < 0.333){
+    return 'rock';
+  } else if (rand < 0.666){
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+function getHumanChoice() {
+  //collect human input for rock paper scissors choice
+  let choice = prompt(`Rock, Paper, or Scissors?`);
+  return console.log(choice);
+}
+
+console.log(`Rock, Paper, Scissors. Let's begin!`);
+getHumanChoice();
+console.log('Computer choice:', getComputerChoice());
